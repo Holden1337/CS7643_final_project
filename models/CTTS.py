b7268701023d2325b14b756e83bb26736c26f66b
holden1337 <holden.ruegger@protonmail.com>
2025-03-15 09:37:15 -0400

    adding data

---
+++ models/CTTS.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+
+
+class CTTS(nn.Module):
+    """
+    Class for our CTTS model
+    """
+    def __init__(self, device, kernel_size=16, stride=8, padding=0, d_model=128, nhead=4, enc_layers=4, dim_feedforward=512, dropout=0.3):
+        super().__init__(*args, **kwargs)
+        # params
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+        self.d_model = d_model
+        self.nhead = nhead
+        self.enc_layers = enc_layers
+        self.dim_feedforward = dim_feedforward
+        self.dropout = dropout
+        self.device = device
+
+
+        # model layers
+        self.cnn_layer = nn.Conv1d(device=self.device, in_channels=1, out_channels=1, kernel_size=self.kernel_size,
+                                    stride=self.stride, padding=self.padding)
+        
+
+        # TODO: We apparently need a positional enocoding layer and we will add the output of this to the output of the cnn
+        # kind of like the transformer project but instead of two embedding layers (word and position) we only need one since
+        # the cnn is doing the "token" embedding
+        # reason i have the TODO is that I'm not sure about the dimensions
+        self.pos_embed_layer = nn.Embedding(num_embeddings=80, embedding_dim=128, device=self.device)
+
+        # transformer layer
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=self.d_model, nhead=self.nhead, dim_feedforward=self.dim_feedforward,
+                                                        dropout=self.dropout, device=self.device)
+        
+        self.transformer_encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=4)
+
+        # final output layer
+        #TODO: might want to experiment with more linear layers
+        # in the paper it just says "MLP" in the diagram, they don't say how many
+        # layers they used.
+        self.final_layer = nn.Linear(in_features=self.d_model, out_features=3)
+
+    
+    def forward(self, x):
+        """
+        forward pass of CTTS model.
+        Should in theory be:
+        1. Pass time series (x) through CNN layer to create token embedding
+        2. Pass token embedding through positional embedding layer to create final embed
+        3. Pass final embed through transformer encoder
+        4. Take outputs from transformer encoder and pass those though MLP layer
+        5. Return outputs from MLP layer
+
+        but we may have to trouble shoot
+
+        :param x: torch tensor of size (figure this out later) time series input batch
+        :return outputs: torch tensor of size (figure this out later) output of CTTS model
+        """
+
+        #token_embed = self.cnn_layer(x)
+        # figure out positional embedding here
+        #pos_embed = self.pos_embed_layer(some_input)
+        #embedding = torch.add(token_embed, pos_embed)
+
+        # transformer_output = self.transformer_encoder(embedding)
+
+        #mlp_output = self.final_layer(transformer_output)
+
+        # return mlp_output
---
+++ scripts/get_stock_data.py
@@ -0,0 +1,85 @@
+import pandas as pd
+from polygon import RESTClient
+from tqdm import tqdm 
+import time
+import logging
+logger = logging.getLogger(__name__)
+
+api_key = "BMd3CY940NNG3r9_reWCzTBbXc1cy2Oi"
+client = RESTClient(api_key)
+
+from_date = "2024-11-29"
+to_date = "2024-12-30"
+
+constituents = pd.read_csv("constituents.csv")
+tickers = list(constituents['Symbol'].values)
+base_dir = '/home/holden/github/OMSCS/DL/stock_data/'
+
+def get_price_df(ticker):
+    """
+    use polygon api to get stock data for year 2024.
+    Different stocks have different amount of data with 
+    some minutes being missing so we can't just uniformly
+    pull all stocks and concat them into a single df. we can  
+    just make a folder and save all the csvs there and pull them later
+    :param ticker: string - stock ticker label
+    :return df: dataframe with stock price info
+    """
+
+    multiplier = 1  # 1-minute intervals
+    timespan = "minute"
+    df = pd.DataFrame()
+    multiplier = 1  # 1-minute intervals
+    timespan = "minute"
+    
+    if len(df) == 0:
+        from_date = "2024-01-01"
+        to_date = "2024-12-31"
+        aggs = client.get_aggs(ticker=ticker, multiplier=multiplier, timespan=timespan, from_=from_date, to=to_date, limit=50000000)
+        temp_df = pd.DataFrame(aggs)
+        # Convert timestamp to datetime
+        temp_df["timestamp"] = pd.to_datetime(temp_df["timestamp"], unit="ms")
+        df = pd.concat([df, temp_df])
+    
+    while df.iloc[-1]['timestamp'] < pd.to_datetime("2025-02-10 00:00:00"):
+        from_date = df.iloc[-1]['timestamp'].strftime("%Y-%m-%d")
+        aggs = client.get_aggs(ticker=ticker, multiplier=multiplier, timespan=timespan, from_=from_date, to=to_date, limit=50000000)
+        temp_df = pd.DataFrame(aggs)
+        
+        temp_df["timestamp"] = pd.to_datetime(temp_df["timestamp"], unit="ms")
+        temp_df_filtered = temp_df[~temp_df['timestamp'].isin(df['timestamp'])]
+        if temp_df_filtered.iloc[-1]['timestamp'] > pd.to_datetime("2024-12-28 00:00:00"):
+            temp_df_filtered = temp_df_filtered[temp_df_filtered['timestamp'] < pd.to_datetime("2024-12-28 00:00:00")]
+            temp_df_filtered = temp_df_filtered.reset_index(drop=True)
+        if len(temp_df_filtered) > 0:
+            df = pd.concat([df, temp_df_filtered], ignore_index=True)
+        else:
+            return df
+        
+
+def main():
+    logging.basicConfig(filename="stock_price_pulling_logging.log", level=logging.INFO)
+    logger.info("Started")
+    num_successes = 0
+    num_fails = 0
+    for ticker in tqdm(tickers):
+        try:
+            df = get_price_df(ticker)
+            save_file_path = base_dir + ticker + ".csv"
+            df.to_csv(save_file_path, index=False)
+            logger.info("Finished pulling data for " + ticker)
+            num_successes += 1
+        except Exception as e:
+            logger.info("Issue pulling data for " + ticker)
+            num_fails += 1
+            pass
+        time.sleep(60)
+    logger.info("Number of stocks successfully pulled: " + str(num_successes))
+    logger.info("Number of stocks that failed to pull: " + str(num_fails))
+
+
+if __name__ == '__main__':
+    main()
+    
+
+
